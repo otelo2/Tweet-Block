b8680a9959fc245587625ed150a7699b91b242b4
Otelo2 <josean2009@live.com.mx>
2020-02-03 13:19:19 -0600

    First commit

---
+++ TweetBlock.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from secrets import username,password
+
+class TweetBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get('https://twitter.com')
+        login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]')
+        login_btn.click()
+        email_in=self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')
+        email_in.send_keys(username)
