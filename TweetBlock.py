91231068f9d0504b48acc7deb97e07d3091ab27e
Otelo2 <josean2009@live.com.mx>
2020-02-03 13:28:13 -0600

    Add login function

--- TweetBlock.py
+++ TweetBlock.py
@@ -9,5 +9,12 @@ class TweetBot():
         self.driver.get('https://twitter.com')
         login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]')
         login_btn.click()
-        email_in=self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')
+        email_in = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')
         email_in.send_keys(username)
+        pass_in = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[2]/label/div[2]/div/input')
+        pass_in.send_keys(password)
+        send_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[3]/div/div')
+        send_btn.click()
+
+    def search(self):
+        a=1
