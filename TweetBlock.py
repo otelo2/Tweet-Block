81fc179a9c7893d0810d2b72f97046646a60e714
Otelo2 <josean2009@live.com.mx>
2020-02-03 13:39:00 -0600

    Optimized login times

--- TweetBlock.py
+++ TweetBlock.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from secrets import username,password
+from time import sleep
 
 class TweetBot():
     def __init__(self):
@@ -7,8 +8,14 @@ class TweetBot():
 
     def login(self):
         self.driver.get('https://twitter.com')
+
+        sleep(10)
+
         login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]')
         login_btn.click()
+
+        sleep(2)
+
         email_in = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')
         email_in.send_keys(username)
         pass_in = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[2]/label/div[2]/div/input')
