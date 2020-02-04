99b89580088e4206b91391d0d04b20ca82b5f94c
Otelo2 <josean2009@live.com.mx>
2020-02-03 19:03:25 -0600

    Search code finally works

--- TweetBlock.py
+++ TweetBlock.py
@@ -14,7 +14,7 @@ class TweetBot():
         login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]')
         login_btn.click()
 
-        sleep(2)
+        sleep(1)
 
         email_in = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[1]/main/div/div/form/div/div[1]/label/div[2]/div/input')
         email_in.send_keys(username)
@@ -28,23 +28,36 @@ class TweetBot():
     def search(self):
         search_bar = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div[2]/input')
         search_bar.send_keys('@ennioft')
-        sleep(1)
+        sleep(6)
+    #    try:
+    #        user_result = self.driver.find_element_by_xpath('//*[@id="typeaheadDropdown-2"]/div[3]/div[2]/div/span')
+    #        user_result.click()
+    #    except Exception:
+    #        #Ugly fix for now
+    #        self.driver.get('https://twitter.com/ennioft')
+    #        print("Used alternative search")
         try:
+            print('Try 1')
             user_result = self.driver.find_element_by_xpath('//*[@id="typeaheadDropdown-2"]/div[3]/div[2]/div/span')
             user_result.click()
         except Exception:
-            #Ugly fix for now
-            self.driver.get('https://twitter.com/ennioft')
-            print("Used alternative search")
-        sleep(1)
+            try:
+                print('Try 2')
+                user_result = self.driver.find_element_by_xpath('//*[@id="typeaheadDropdown-2"]/div[3]/div[1]/div/div[1]')
+                user_result.click()
+            except Exception:
+                print('Try 3')
+                user_result = self.driver.find_element_by_xpath('/html/body/div/div/div/div/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[2]/div/div[3]/div[1]')
+                user_result.click()
+        print('Ended search')
+        sleep(2)
 
     def getFollowing(self):
         following_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[2]/span')
         following_btn.click()
-        sleep(0.5)
+        sleep(1)
 
     def test(self):
         self.login()
-        self.search()
         self.search()
         self.getFollowing()
